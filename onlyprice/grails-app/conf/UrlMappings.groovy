ef4765fa8c7f0011abb2d34661ad386dbfaa9f4e
nbaertuo <nbaertuo@b8ed4fb8-54c2-11de-bb24-2d5e9b4af41d>
2011-05-30 07:54:15 +0000

    修改首页跳转

--- onlyprice/grails-app/conf/UrlMappings.groovy
+++ onlyprice/grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,11 @@
 class UrlMappings {
 
-	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
+    static mappings = {
+        "/$controller/$action?/$id?"{ constraints {
+                // apply constraints here
+            } }
 
-		"/"(view:"/index")
-		"500"(view:'/error')
-	}
+        "/"(controller:"bid",action:"onTime")
+        "500"(view:'/error')
+    }
 }
